e84e13d040bdee66a54612ffb3b605619e1c2804
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-06-11 10:43:26 -0400

    update get requests

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require("morgan")
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models/exercise");
+const db = require("./models");
 
 const path = require('path');
 
@@ -17,10 +17,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercise", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,});
 
+// get requests   
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/exercise.html"));
+});
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/stats.html"));
+});
 
 app.listen(PORT, () => {
 console.log(`App running on port ${PORT}!`);
