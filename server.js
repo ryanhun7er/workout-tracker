06ef67c411aabc97c82d2bcbb8ac47382e82dfa4
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-06-11 11:06:12 -0400

    update exercise.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,});
 
+
 // get requests   
 
 app.get("/", (req, res) => {
@@ -30,6 +31,42 @@ app.get("/exercise", (req, res) => {
 });
 app.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname + "./public/stats.html"));
+});
+
+// get request for last 7 days
+app.get("/api/workouts/range", (req, res) => {
+  db.find({})
+  .limit(7)
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+//call to create new workout
+
+// get a workout by id and then push an excercise to the array
+app.put("/api/workouts/:id", ({body, params}, res) => {
+  db.findByIdAndUpdate(params.id, {$push: {exercises: body}}, {new: true, runValidators: true})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+});
+
+// function to post a workout
+app.post("/api/workouts", (req, res) => {
+  db.create({})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  });
 });
 
 app.listen(PORT, () => {
